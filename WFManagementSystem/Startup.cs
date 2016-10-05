61274b21a2f2c06ac74c1c9f24aaef90836eb862
Jan Matička <j.maticka@protank.cz>
2016-10-05 06:59:15 +0200

    solution added

---
+++ WFManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WFManagementSystem.Startup))]
+namespace WFManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
