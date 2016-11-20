8dba3ddd4dc759c8b2639261576bdea17c06be15
Jan Matička <j.maticka@protank.cz>
2016-11-20 14:19:04 +0100

    api

---
+++ WFManagementSystem/Controllers/api/WorkflowsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WFMDatabase.DML;
+using WFMDatabase.Entities;
+
+namespace WFManagementSystem.Controllers.api
+{
+    public class WorkflowsController : ApiController
+    {
+        IDMLWorkflow dbWorklfow = new DMLWorkflow();
+
+        // POST api/<controller>
+        [Route("api/workflows/create")]
+        public IHttpActionResult Create([FromBody]Workflow value)
+        {
+
+            return Ok();
+        }
+
+    }
+}
